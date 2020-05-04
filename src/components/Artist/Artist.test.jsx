373ba9f10e035f0103b58bcc6ee970a94a1cc3cf
Chelsea Spangler <chelseanspangler@gmail.com>
2020-05-04 16:11:12 -0700

    passing Artist snapshot test

--- src/components/Artist/Artist.test.jsx
+++ src/components/Artist/Artist.test.jsx
@@ -6,12 +6,14 @@ describe('Artist component', () => {
   it('renders Artist', () => {
     const wrapper = shallow(<Artist releases={[
       {
-        songId: 1234,
-        songTitle: 'Help'
+        releaseId: 1234,
+        releaseTitle: 'title',
+        releaseDate: '1995-07-11'
       },
       {
-        songId: 1235,
-        songTitle: 'Halp'
+        releaseId: 1235,
+        releaseTitle: 'title2',
+        releaseDate: '1997-07-12'
       }
     ]}/>);
     expect(wrapper).toMatchSnapshot();
