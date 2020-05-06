df6df717ceeb448745f8b899ae0d7fd6d96fca14
Chelsea Spangler <chelseanspangler@gmail.com>
2020-05-05 22:04:45 -0700

    default image replaces broken cover art

--- src/components/Discography/Discography.jsx
+++ src/components/Discography/Discography.jsx
@@ -6,7 +6,7 @@ const Discography = ({ artistName, releases }) => {
   const releaseElements = releases.map(release => (
     <li key={release.releaseId}>
       <Link to={`/releases/${artistName}/${release.releaseId}`}>
-        <img src={`http://coverartarchive.org/release/${release.releaseId}/front`} />
+        <img src={`http://coverartarchive.org/release/${release.releaseId}/front`} onError={(e)=>{e.target.onerror = null; e.target.src = 'https://vassifer.blogs.com/.a/6a00d8341c18b253ef0191048c97fb970c-800wi';}}/>
         <span>{release.releaseTitle}: {release.releaseDate}</span>
       </Link>
     </li>
